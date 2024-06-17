c707f7b1b1dd8b21236e00f53afa6a4e4824332e
KOSASIH <kosasihg88@gmail.com>
2024-06-17 18:16:36 +0700

    Create TrainingData.js

---
+++ anomaly-detection/TrainingData.js
@@ -0,0 +1,16 @@
+// TrainingData.js
+class TrainingData {
+  constructor() {
+    this.data = [];
+  }
+
+  addDataPoint(point) {
+    // Add a new data point to the training data
+  }
+
+  generate() {
+    // Generate training data for the anomaly detection model
+  }
+}
+
+module.exports = TrainingData;
