7b3129a32dedcf57aa779586f15dcf0cebb67bd4
KOSASIH <kosasihg88@gmail.com>
2024-06-17 18:37:53 +0700

    Create EncryptedDataStorage.py

---
+++ homomorphic-encryption/EncryptedDataStorage.py
@@ -0,0 +1,14 @@
+# EncryptedDataStorage.py
+from.HE import HomomorphicEncryption
+
+class EncryptedDataStorage:
+    def __init__(self):
+        self.he = HomomorphicEncryption()
+
+    def store_data(self, data):
+        # Encrypt and store data
+        pass
+
+    def retrieve_data(self):
+        # Retrieve and decrypt data
+        pass
