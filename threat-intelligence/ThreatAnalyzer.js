4418983b027ea04f31b37b619d10273e9fc02304
KOSASIH <kosasihg88@gmail.com>
2024-06-17 18:32:37 +0700

    Create ThreatAnalyzer.js

---
+++ threat-intelligence/ThreatAnalyzer.js
@@ -0,0 +1,14 @@
+// ThreatAnalyzer.js
+const { GNNModule } = require('./GNNModule');
+
+class ThreatAnalyzer {
+  constructor() {
+    this.gnnModule = new GNNModule();
+  }
+
+  analyzeThreats(threatData) {
+    // Use GNN module to analyze threats in real-time
+  }
+}
+
+module.exports = ThreatAnalyzer;
