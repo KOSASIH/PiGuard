dec9b1286c8698421a4f45536406abdbbb9fd68f
KOSASIH <kosasihg88@gmail.com>
2024-06-17 17:40:06 +0700

    Create reputationCalculator.js

---
+++ backend/utils/reputationCalculator.js
@@ -0,0 +1,8 @@
+// reputationCalculator.js
+const calculateReputation = (node) => {
+  // TO DO: implement reputation calculation logic
+  // For now, return a random reputation score between 0 and 100
+  return Math.floor(Math.random() * 101);
+};
+
+module.exports = calculateReputation;
